6688767046f92851f0a78a21ee0675729fe7c67f
Jimmy Börjesson <jimmy@boerjesson.se>
2020-12-25 19:55:16 +0100

    Add an advanced puzzle to the examples

--- src/lib/puzzles.ts
+++ src/lib/puzzles.ts
@@ -2,7 +2,7 @@ import { createPuzzle } from "./solver";
 
 const U = undefined;
 
-export const tenByTen = createPuzzle([
+export const basic10x10 = createPuzzle([
   [U, 2, 3, U, U, 0, U, U, U, U],
   [U, U, U, U, 3, U, 2, U, U, 6],
   [U, U, 5, U, 5, 3, U, 5, 7, 4],
@@ -15,7 +15,7 @@ export const tenByTen = createPuzzle([
   [U, 4, 4, U, U, U, U, 4, U, U],
 ]);
 
-export const tenByFifteen = createPuzzle([
+export const basic10x15 = createPuzzle([
   [0, U, U, U, U, U, 5, 6, U, U],
   [U, 2, U, 3, U, U, U, 8, 9, U],
   [U, U, 3, U, 2, 2, U, 7, 8, 6],
@@ -33,7 +33,21 @@ export const tenByFifteen = createPuzzle([
   [0, 1, U, U, U, 4, U, U, U, U],
 ]);
 
+export const advanced10x10 = createPuzzle([
+  [U, U, 3, 3, U, U, U, U, U, U],
+  [3, U, U, U, U, U, 0, U, 0, U],
+  [U, U, 3, 4, U, 3, U, U, U, U],
+  [3, U, 4, U, U, U, U, 3, U, U],
+  [2, 3, U, 5, U, 4, 4, U, U, 4],
+  [U, U, 5, 4, 6, 6, U, 4, U, 4],
+  [U, U, U, U, U, 3, 3, U, U, 4],
+  [U, 3, U, U, 5, 6, 5, U, U, 4],
+  [U, U, U, 7, U, U, U, 7, U, 5],
+  [U, 4, U, U, 6, U, 6, U, 5, U],
+]);
+
 export const examples = [
-  tenByTen,
-  tenByFifteen,
+  basic10x10,
+  basic10x15,
+  advanced10x10,
 ];
