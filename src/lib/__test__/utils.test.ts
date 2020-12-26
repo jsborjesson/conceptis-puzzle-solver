a51e314a6ad5ad3471b942076021b989a62762fe
Jimmy Börjesson <jimmy@boerjesson.se>
2020-12-26 12:00:22 +0100

    Implement difference and intersection

--- src/lib/__test__/utils.test.ts
+++ src/lib/__test__/utils.test.ts
@@ -1,4 +1,4 @@
-import { deepCopy } from "../utils";
+import { deepCopy, difference, intersection } from "../utils";
 
 describe("deepCopy", () => {
   test("changing a copy does not alter original", () => {
@@ -12,5 +12,25 @@ describe("deepCopy", () => {
     copy[0][0] = 100;
 
     expect(original[0][0]).toEqual(1);
+  });
+});
+
+describe("difference", () => {
+  test("returns difference between two arrays", () => {
+    const a = [1, 2, 3];
+    const b = [2, 3, 4];
+
+    expect(difference(a, b)).toEqual([1]);
+    expect(difference(b, a)).toEqual([4]);
+  });
+});
+
+describe("intersection", () => {
+  test("returns intersection between two arrays", () => {
+    const a = [1, 2, 3];
+    const b = [2, 3, 4];
+
+    expect(intersection(a, b)).toEqual([2, 3]);
+    expect(intersection(b, a)).toEqual([2, 3]);
   });
 });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
 export const deepCopy = <T>(obj: T): T => {
   return JSON.parse(JSON.stringify(obj));
 };
+
+export const difference = <T>(a: T[], b: T[]): T[] => {
+  return a.filter(item => !b.includes(item));
+};
+
+export const intersection = <T>(a: T[], b: T[]): T[] => {
+  return a.filter(item => b.includes(item));
+};
