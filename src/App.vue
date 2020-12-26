e600efa6d3d265ca44f96aabc6308ce067b8ee08
Jimmy Börjesson <jimmy@boerjesson.se>
2020-12-26 17:11:55 +0100

    Fix spacing between elements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1 class="mt-8 text-5xl text-center">fill-a-pix solver</h1>
   <FillAPix />
 </template>
 
--- src/components/FillAPix.vue
+++ src/components/FillAPix.vue
@@ -1,43 +1,47 @@
 <template>
-  <div class="flex flex-col items-center justify-center my-8 space-y-4">
-    <span>Input a puzzle</span>
+  <div class="px-4 py-12 space-y-8">
+    <h1 class="text-5xl text-center">Fill-a-Pix Solver</h1>
 
-    <div class="flex items-center justify-center space-x-4">
-      <input type="number" v-model="height" class="w-20 p-2 border border-gray-400 rounded" />
-      <span>x</span>
-      <input type="number" v-model="width" class="w-20 p-2 border border-gray-400 rounded" />
-      <button @click="blankPuzzle" class="p-2 bg-gray-100 border border-gray-400 rounded active:bg-gray-300">Clear puzzle</button>
+    <div class="flex flex-col items-center space-y-4">
+      <span>Input a puzzle</span>
+
+      <div class="flex items-center justify-center space-x-4">
+        <input type="number" v-model="height" class="w-20 p-2 border border-gray-400 rounded" />
+        <span>x</span>
+        <input type="number" v-model="width" class="w-20 p-2 border border-gray-400 rounded" />
+        <button @click="blankPuzzle" class="p-2 bg-gray-100 border border-gray-400 rounded active:bg-gray-300">Clear puzzle</button>
+      </div>
+
+      <span>or start with an example</span>
+
+      <div class="flex items-center justify-center space-x-4">
+        <select v-model="example" class="p-2 border border-gray-400 rounded">
+          <option v-for="(_, name) in examples" :key="name" :value="name">{{ name }}</option>
+        </select>
+        <button @click="examplePuzzle(example)" class="p-2 bg-gray-100 border border-gray-400 rounded active:bg-gray-300">Show</button>
+      </div>
     </div>
 
-    <span>or start with an example</span>
+    <!-- Grid -->
+    <table class="mx-auto border-t border-l border-black">
+      <tr v-for="(rows, row) in puzzle" :key="row">
+        <td
+          v-for="(square, col) in rows"
+          :key="row + 'x' + col"
+          class="w-12 h-12 text-center border-b border-r border-black"
+          :class="{'bg-black text-white': square.fill === 'filled', 'bg-gray-300': square.fill === 'crossed'}">
+          <input
+            class="w-full text-center bg-transparent"
+            v-model="puzzle[row][col].number" />
+        </td>
+      </tr>
+    </table>
 
-    <div class="flex items-center justify-center space-x-4">
-      <select v-model="example" class="p-2 border border-gray-400 rounded">
-        <option v-for="(_, name) in examples" :key="name" :value="name">{{ name }}</option>
-      </select>
-      <button @click="examplePuzzle(example)" class="p-2 bg-gray-100 border border-gray-400 rounded active:bg-gray-300">Show</button>
+    <!-- Solver buttons -->
+    <div class="flex">
+      <button class="flex-1 p-4 font-bold text-white bg-red-500 rounded-l active:bg-red-700" @click="stepBack">Step back</button>
+      <button class="flex-1 p-4 font-bold text-white bg-green-500 rounded-r active:bg-green-700" @click="stepForward">Step forward</button>
     </div>
-  </div>
-
-  <!-- Grid -->
-  <table class="mx-auto my-8 border-t border-l border-black">
-    <tr v-for="(rows, row) in puzzle" :key="row">
-      <td
-        v-for="(square, col) in rows"
-        :key="row + 'x' + col"
-        class="w-12 h-12 text-center border-b border-r border-black"
-        :class="{'bg-black text-white': square.fill === 'filled', 'bg-gray-300': square.fill === 'crossed'}">
-        <input
-          class="w-full text-center bg-transparent"
-          v-model="puzzle[row][col].number" />
-      </td>
-    </tr>
-  </table>
-
-  <!-- Solver buttons -->
-  <div class="flex px-4 mb-12">
-    <button class="flex-1 p-4 font-bold text-white bg-red-500 rounded-l active:bg-red-700" @click="stepBack">Step back</button>
-    <button class="flex-1 p-4 font-bold text-white bg-green-500 rounded-r active:bg-green-700" @click="stepForward">Step forward</button>
   </div>
 </template>
 
